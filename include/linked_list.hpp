97b697027ad5ba37956bad6fddac32135fd3c406
Phong Do <Donguyenphong@icloud.com>
2025-06-08 00:31:41 -0700

    completed simple easy impls for single linked lists

--- include/linked_list.hpp
+++ include/linked_list.hpp
@@ -8,6 +8,7 @@ private:
   T data;
   Node *next;
 
+public:
   Node() : data(), next(nullptr) {}
   Node(T data) : data(data), next(nullptr) {}
   Node(T data, Node *next) : data(data), next(next) {}
@@ -34,44 +35,117 @@ public:
   }
 
   void push_front(const T &data) {
-    Node<T> *newHead = new Node<T>(data, head);
-    head = newHead;
-  }
-  void push_back(const T &data) {
-    Node<T> *current = head;
-    while (current && current->next) {
-      current = current->next;
+    if (!head) {
+      head = new Node<T>(data);
+      return;
     }
-    current->next = new Node<T>(data);
+    head = new Node<T>(data, head);
   }
-  void pop_front(void) {
-    Node<T> *temp = head->next;
-    delete head;
-    head = temp;
-  }
-  void pop_back(void) {
-    Node<T> *current = head;
-    while (current && current->next) {
-      current = current->next;
-    }
-    delete current;
-  }
-  void insert_at(size_t index, T value) {
-    Node<T> *current = head;
-    for (size_t i = 0; i < index - 1; ++i) {
-      if (!current)
-        return;
-      current = current->next;
-    }
-    current->next = new Node<T>(value, current->next->next);
-  }
-  void remove_at(size_t index) {}
-  T *find(T value) {}
 
-  T *begin(void) {}
-  T *end(void) {}
-  T *cbegin(void) const {}
-  T *cend(void) const {}
+  void push_back(const T &data) {
+    if (!head) {
+      head = new Node<T>(data);
+      return;
+    }
+    Node<T> *curr = head;
+    while (curr->next) {
+      curr = curr->next;
+    }
+    curr->next = new Node<T>(data);
+  }
+
+  void pop_front(void) {
+    if (!head) {
+      return;
+    }
+    Node<T> *temp = head;
+    head = head->next;
+    delete temp;
+  }
+
+  void pop_back(void) {
+    if (!head) {
+      return;
+    }
+    if (!head->next) {
+      delete head;
+      head = nullptr;
+      return;
+    }
+    Node<T> *curr = head;
+    while (curr->next && curr->next->next) {
+      curr = curr->next;
+    }
+    Node<T> *temp = curr->next;
+    curr->next = nullptr;
+    delete temp;
+  }
+
+  void insert_at(const size_t index, const T value) {
+    if (index == 0) {
+      push_front(value);
+      return;
+    }
+    Node<T> *curr = head;
+    for (size_t i = 0; curr && i < index - 1; i++) {
+      curr = curr->next;
+    }
+    if (!curr) {
+      return; // Index out of bounds
+    }
+    curr->next = new Node<T>(value, curr->next);
+  }
+
+  void remove_at(const size_t index) {
+    if (!head)
+      return;
+    if (index == 0) {
+      pop_front();
+      return;
+    }
+    Node<T> *curr = head;
+    for (size_t i = 0; curr->next && i < index - 1; i++) {
+      curr = curr->next;
+    }
+    if (!curr || !curr->next) {
+      return; // Index out of bounds
+    }
+    Node<T> *temp = curr->next;
+    curr->next = temp->next;
+    delete temp;
+  }
+
+  T *find(const T value) {
+    if (!head) {
+      return nullptr;
+    }
+    Node<T> *curr = head;
+    while (curr) {
+      if (curr->data == value) {
+        return &curr->data;
+      }
+      curr = curr->next;
+    }
+    return nullptr;
+  }
+
+  T *begin(void) { return head; }
+
+  T *end(void) { return nullptr; }
+
+  T *cbegin(void) const { return head; }
+
+  T *cend(void) const { return nullptr; }
+
+  size_t size(void) const {
+    size_t count = 0;
+    Node<T> *curr = head;
+    while (curr) {
+      count++;
+      curr = curr->next;
+    }
+    return count;
+  }
 };
 
 #endif
